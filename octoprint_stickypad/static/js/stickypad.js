285ca34712caf437e4cff5e9b19e793cf484e3b9
jneilliii <jneilliii+github@gmail.com>
2020-06-05 02:54:49 -0400

    0.1.3 (#8)
    * Switched to WYSIWIG editor
    * Changed to navbar button with slide out panel
    * Bump version to prevent update loop

--- octoprint_stickypad/static/js/stickypad.js
+++ octoprint_stickypad/static/js/stickypad.js
@@ -9,54 +9,42 @@ $(function() {
 		var self = this;
 
 		self.settingsViewModel = parameters[0];
+		self.loginStateViewModel = parameters[1];
+		self.note = ko.observable();
+		self.saving = ko.observable(false);
 
 		self.onBeforeBinding = function(){
-			var note = self.settingsViewModel.settings.plugins.stickypad.note();
-			new PNotify({
-				title: 'Sticky Pad',
-				text: '<textarea class="stickypad_text">' + note + '</textarea>',
-				hide: false,
-				icon: 'icon icon-exclamation',
-				addclass: 'stickypad',
-				min_height: (window.innerHeight - 80) + 'px',
-				confirm: {
-					confirm: true,
-					buttons: [{
-							text: 'Save',
-							addClass: 'btn',
-							click: function(notice) {
-										OctoPrint.settings.savePluginSettings('stickypad', {'note':$('div.ui-pnotify.stickypad > div > div.ui-pnotify-text > textarea.stickypad_text').val()});
-									}
-						},
-						{
-							text: 'Cancel',
-							addClass: 'hidden',
-							click: function(notice) {
-										notice.remove();
-									}
-						},
+			self.note(self.settingsViewModel.settings.plugins.stickypad.note);
+			self.quill = new Quill('#editor-container', {theme: 'snow'});
+		}
 
-						]
-				},
-				buttons: {
-					closer: false,
-					sticker: false
-				},
-				history: {
-					history: false
-				}
+		self.showEditor = function(data){
+			$('#navbar_plugin_stickypad').addClass('open');
+			$('#stickypadbuttonpanel').slideDown('slow');
+		}
+
+		self.closeEditor = function(data) {
+			self.quill.setContents(ko.toJS(self.note()));
+			$('#stickypadbuttonpanel').slideUp('slow', function(){$('#navbar_plugin_stickypad').removeClass('open');});
+		}
+
+		self.saveNote = function(data){
+			self.saving(true);
+			OctoPrint.settings.savePluginSettings('stickypad', {'note': self.quill.getContents()}).done(function(){
+					$('#stickypadbuttonpanel').slideUp('slow', function(){$('#navbar_plugin_stickypad').removeClass('open');
+					self.saving(false);
+				});
 			});
 		}
-		
-		self.onStartupComplete = function(){
-			$('div.ui-pnotify.stickypad > div > div.ui-pnotify-text').height(function(){return parseInt($(this).parent().height() - 40) + 'px'});
-			$('div.ui-pnotify.stickypad > div > div.ui-pnotify-text').width(function(){return parseInt($(this).parent().width()) + 'px'});
+
+		self.onAllBound = function(){
+			self.quill.setContents(ko.toJS(self.note()));
 		}
 	}
 
 	OCTOPRINT_VIEWMODELS.push({
 		construct: StickypadViewModel,
-		dependencies: ['settingsViewModel'],
-		elements: []
+		dependencies: ['settingsViewModel','loginStateViewModel'],
+		elements: ['#navbar_plugin_stickypad','#stickypadbuttonpanel']
 	});
 });
